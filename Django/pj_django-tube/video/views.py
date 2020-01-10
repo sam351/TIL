fb05823674e7ec2caed0d460712ef728e657921f
sam <ksmo9191@hanmail.net>
2020-01-10 22:04:04 +0900

    Modify views.py (Create View)

--- Django/pj_django-tube/video/views.py
+++ Django/pj_django-tube/video/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from .model import Video
 
 # Create your views here.
+def video_list(request):
+    video_list = Video.objects.all()
+    return render(request, 'video/video_list.html', {'video_list':video_list})

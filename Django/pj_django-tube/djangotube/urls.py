314edcb35836ff7dfb36139ea2d4b147a4d07f48
sam <ksmo9191@hanmail.net>
2020-01-10 22:43:19 +0900

    Modify djangotube/urls.py (Debug)

--- Django/pj_django-tube/djangotube/urls.py
+++ Django/pj_django-tube/djangotube/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^video/', include('video.urls', namespace='video')),
+    url(r'^video/', include(('video.urls', 'video'), namespace='video')),
 ]

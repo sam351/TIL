dbe71187b852bb7517fdf7eb325638fb906a3f4e
sam <ksmo9191@hanmail.net>
2020-01-11 16:34:53 +0900

    Modify urls.py, views.py, Add video_new.html (Add new video)

--- Django/pj_django-tube/video/urls.py
+++ Django/pj_django-tube/video/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.video_list, name='list')
+    url(r'^$', views.video_list, name='list'),
+    url(r'^new$', views.video_new, name='new')
 ]
--- Django/pj_django-tube/video/views.py
+++ Django/pj_django-tube/video/views.py
@@ -6,3 +6,12 @@ from .models import Video
 def video_list(request):
     video_list = Video.objects.all()
     return render(request, 'video/video_list.html', {'video_list':video_list})
+
+def video_new(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        video_key = request.POST['video_key']
+        Video.objects.create(title = title, video_key = video_key)
+        return redirect(reverse('video:list'))
+    elif request.method == "GET":
+        return render(request, 'video/video_new.html')
